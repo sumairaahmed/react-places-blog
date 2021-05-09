130c2d8b998897970024b9fe0e03518bbe69f981
sumairaahmed <sumairaahmedcp@gmail.com>
2021-05-09 21:38:53 +0530

    setup, pages and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom'
 import './App.css';
+import Users from './users/pages/Users';
+import NewPlaces from './places/pages/NewPlaces';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Works!</h1>
-    </div>
+    
+    <Router>
+      <Switch>
+      <Route path='/' exact>
+        <Users />
+      </Route>
+      <Route path='/places/new' exact>
+        <NewPlaces />
+      </Route>
+      
+      <Redirect to='/' />
+      </Switch>
+      
+    </Router>
+    
   );
 }
 
